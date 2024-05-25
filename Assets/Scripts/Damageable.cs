c2d2347851b42c3de818ae99f192250d1a15caa2
Hafidz Maulana <hafidz.maulana465@gmail.com>
2024-05-26 03:12:12 +0700

    Add Healing Pickup Item

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -131,7 +131,7 @@ public class Damageable : MonoBehaviour
             float maxHeal = Mathf.Max(MaxHealth - Health, 0);
             float actualHeal = Mathf.Min(maxHeal, healthRestore);
             Health += actualHeal;
-            // CharacterEvents.characterHealed(gameObject, actualHeal);
+            CharacterEvents.characterHealed(gameObject, actualHeal);
             return true;
         }
         return false;
